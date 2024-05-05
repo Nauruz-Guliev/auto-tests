ec1b125a1935ee5daf5cfcff3b6af02e6f5b5f63
Nauruz Guliev <NGuliev@alfabank.ru>
2024-05-05 18:47:33 +0300

    delete test added

--- src/test/java/ru/kpfu/itis/helper/DeleteHelper.java
+++ src/test/java/ru/kpfu/itis/helper/DeleteHelper.java
@@ -8,4 +8,9 @@ public class DeleteHelper extends HelperBase {
     public DeleteHelper(AppManager app) {
         super(app);
     }
+
+    public void deleteNote() {
+        getElementByLinkText("Delete Note").click();
+        app.getDriver().switchTo().alert().accept();
+    }
 }
--- src/test/java/ru/kpfu/itis/test/DeleteTest.java
+++ src/test/java/ru/kpfu/itis/test/DeleteTest.java
@@ -1,5 +1,28 @@
 package ru.kpfu.itis.test;
 
-public class DeleteTest {
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import ru.kpfu.itis.helper.ChangeHelper;
+import ru.kpfu.itis.helper.CreateHelper;
+import ru.kpfu.itis.helper.DeleteHelper;
+import ru.kpfu.itis.helper.NavigationHelper;
+import ru.kpfu.itis.mapper.ValidDataMapper;
+import ru.kpfu.itis.model.TestData;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class DeleteTest extends AuthBase {
+
+    @ParameterizedTest
+    @ArgumentsSource(ValidDataMapper.class)
+    public void test_note_change(TestData testData) {
+        NavigationHelper navigationHelper = app.getNavigationHelper();
+        CreateHelper createHelper = app.getCreateHelper();
+        DeleteHelper deleteHelper = app.getDeleteHelper();
+        navigationHelper.goMainPage(testData.getBaseUrl());
+        createHelper.createNote(testData.getNoteText(), testData.getNoteTitle());
+        deleteHelper.deleteNote();
+        boolean isNotePresent = deleteHelper.checkElementExistsLinkText(testData.getNoteTitle());
+        assertFalse(isNotePresent);
+    }
 }
